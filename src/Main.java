15074f15e6eb6110b6a9d37e603c40ba74cacadc
Tran Nam Tuan <tuantran1632001@gmail.com>
2020-09-30 14:58:31 +0700

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a logger
+        Logger logger = Logger.getLogger(Main.class.getName());
+        logger.log(Level.INFO, "Hello, world!");
     }
 }
