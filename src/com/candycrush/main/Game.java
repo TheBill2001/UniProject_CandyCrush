b380d8472ad461670cc9630e30e1e78c6327657c
Tran Nam Tuan <tuantran1632001@gmail.com>
2020-10-08 18:39:07 +0700

    Added JFrame

---
+++ src/com/candycrush/main/Game.java
@@ -0,0 +1,20 @@
+package com.candycrush.main;
+
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable {
+    private static final long serialVersionUID = 8102020L;
+
+    public synchronized void start() {
+
+    }
+
+    public void run() {
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+    
+}
